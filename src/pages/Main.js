275d50ddb1cd755c3e75c4d655d60de8763891ff
adamrossbenton <adamrossbenton@gmail.com>
2022-02-08 01:30:01 -0800

    still gotta fix the font a little bit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,27 @@
 import React from "react"
+import "../App.css"
 
 function Main(props) {
     
+    const spingBoy = {
+        margin: "0 auto",
+        height: "480px",
+        width: "700px",
+        background: "url(spingboy.png)",
+        backgroundSize: "auto",
+        display: "flex",
+        justifyContent: "center",
+    }
+
+    // const memeText = {
+    //     // marginTop: "auto",
+    //     // color: "white",
+    //     // textStroke: "2px black",
+    //     // fontSize: "2.5em",
+    //     // fontWeight: "1000",
+    //     // fontFamily: "Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif"
+    // }
+
     return <div>
         <h1>Who lives in a pineapple under the sea?</h1>
         <h1>I wrote this dumb program to generate memes!</h1>
@@ -10,7 +30,9 @@ function Main(props) {
             <input type="text" placeholder="undefined" name="text" onChange={props.handleChange} />
         </form>
         <h2>Looks like:</h2>
-        <h3>{props.sarcasticase(`${props.text.text}`)}</h3>
+        <div style={spingBoy}>
+            <h3 className="meme-text">{props.sarcasticase(`${props.text.text}`)}</h3>
+        </div>
     </div>
     
 }
